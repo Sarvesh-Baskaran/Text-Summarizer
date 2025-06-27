589d7ee9884cdb38bd481cd4cdfb596413499156
Sarvesh Baskaran <114791675+Sarvesh-Baskaran@users.noreply.github.com>
2025-06-27 16:05:05 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
 # wsgi.py
 from app import app
+import os
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port or default to 5000
+    app.run(host='0.0.0.0', port=port)
