947d11d9817b761b37fe2bd9f4e7d5f2b82de10c
Sarvesh Baskaran <114791675+Sarvesh-Baskaran@users.noreply.github.com>
2025-06-28 16:11:53 +0530

    adding comments and error messages in app.py

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ app = Flask(__name__)
 CORS(app)  # enables CORS for frontend to access API
 
 #load model and tokenizer (you can cache this to avoid reloading every time)
-api_token = os.getenv("HF_TOKEN") #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
+#HF_TOKEN mentioned as a environment variable in railway.com
+api_token = os.getenv("HF_TOKEN") #HF_TOKEN = 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
 #endpoint for model
 API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
 headers = {"Authorization": f"Bearer {api_token}"}
 
+#summarize function to call in input.js
 @app.route('/summarize', methods=['POST'])
 def summarize():
     data = request.get_json()
@@ -24,6 +26,7 @@ def summarize():
     response = requests.post(API_URL, headers=headers, json={"inputs": text})
 
     if response.status_code != 200:
+        print("HuggingFace error: ", response.status_code, response.text)
         return jsonify({
             "error": "Failed to summarize",
             "details": response.text
@@ -33,6 +36,7 @@ def summarize():
         summary_text = response.json()[0]['summary_text']
         return jsonify({"summary": summary_text})
     except Exception as e:
+        print("Parsing error: ",e, response.text)
         return jsonify({
             "error": "Unexpected response format",
             "details": str(e)
